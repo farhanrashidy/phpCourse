e5027a709569562b7ddc9720557877f4efbd93b3
Sheikh Mohammad Farhan Rashidy <sheikh.rashidy02@gmail.com>
2025-04-19 16:15:11 -0400

    varaibles

--- site.php
+++ site.php
@@ -7,7 +7,14 @@
     <body>
 
         <?php
-          echo("Hello World!");
+            $characterName = "Farhan";
+            $characterAge = 21;
+
+            echo "There once was a man name $characterName <br>";
+            echo "He was $characterAge years old <br>";
+            $characterName = "Rashidy";
+            echo "He really liked the name $characterName <br>";
+            echo "But didn't like being $characterAge <br>";
         ?>
 
     </body>
