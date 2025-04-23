39c09d08d5f1a98f781fc6b121ddb740956b1922
Sheikh Mohammad Farhan Rashidy <sheikh.rashidy02@gmail.com>
2025-04-23 17:59:20 -0400

    second commit

--- site.php
+++ site.php
@@ -7,14 +7,25 @@
     <body>
 
         <?php
-            $characterName = "Farhan";
+            /*$characterName = "Farhan";
             $characterAge = 21;
 
             echo "There once was a man name $characterName <br>";
             echo "He was $characterAge years old <br>";
             $characterName = "Rashidy";
             echo "He really liked the name $characterName <br>";
-            echo "But didn't like being $characterAge <br>";
+            echo "But didn't like being $characterAge <br>";*/
+            /*
+            $phrase = "to be or not to be"; // string
+            $age = 30; // integers
+            $gpa = 2.1; // float
+            $isMale = true; // boolean
+            echo "Hello<br>" ;
+            echo "$phrase <br>";
+            */
+
+
+
         ?>
 
     </body>
